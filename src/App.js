374dae623b6c1baaf4668351958a3e6c161fc35d
tchangai <tchangaitoyouyoma@gmail.com>
2022-03-18 16:16:43 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <div className="container mt-5">
+  <form>
+      <div className="mb-3">
+        <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+        </div>
+      <div className="mb-3">
+        <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+        <input type="password" className="form-control" id="exampleInputPassword1"/>
+      </div>
+      <div className="mb-3 form-check">
+        <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
+        <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+      </div>
+      <button type="submit" className="btn btn-primary">Submit</button>
+
+  </form>
+    </div>
+  );
+}
+
+export default App;
